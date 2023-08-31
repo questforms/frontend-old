f4dc99199ee8599a99e7138b25dee7723d7a90df
Jacob Bradley <jtbradley1@gmail.com>
2023-08-30 20:57:50 -0700

    feat: make button primary color

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button class="card__button" type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -34,5 +34,9 @@ const count = ref(0)
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.card .card__button {
+  background-color: var(--primary-color);
 }
 </style>
